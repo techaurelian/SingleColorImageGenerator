e437c2d5af5b34cd00e4ada48d7aafe8140ed4aa
techaurelian <dev@techaurelian.com>
2020-08-30 15:22:08 +0300

    First commit

---
+++ SingleColorImageGenerator/app/src/main/java/com/techaurelian/singlecolorimagecreator/NamedColors.kt
@@ -0,0 +1,11 @@
+package com.techaurelian.singlecolorimagecreator
+
+class NamedColors {
+
+}
+
+enum class NamedColor(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF)
+}
